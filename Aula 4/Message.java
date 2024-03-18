ff66d82a5a6159413ca1990680c01fd4004fd92b
vnvz <vvv@cesar.school>
2024-03-18 04:18:17 -0300

    finalização da atividade, ainda faltam testes

--- Aula 4/Message.java
+++ Aula 4/Message.java
@@ -2,31 +2,19 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Message {
-    private String body;
-    private LocalDateTime sendTime;
     private String topic;
+    private String body;
+    private LocalDateTime timestamp;
 
-    public Message(String body, LocalDateTime sendTime, String topic) {
-        this.body = body;
-        this.sendTime = sendTime;
+    public Message(String topic, String body) {
         this.topic = topic;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public LocalDateTime getSendTime() {
-        return sendTime;
-    }
-
-    public String getTopic() {
-        return topic;
+        this.body = body;
+        this.timestamp = LocalDateTime.now();
     }
 
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm");
-        return "[" + sendTime.format(formatter) + "] " + topic + " : " + body;
+        return "[" + timestamp.format(formatter) + "] " + topic + " : " + body;
     }
 }
